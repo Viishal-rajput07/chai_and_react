f1860d378f68c220b77246126f8f8ffc8705173f
Viishal-rajput07 <rajputanavishal7@gmail.com>
2024-04-13 19:46:20 +0530

    themeChanger with contextapi

---
+++ 09themeswitcher/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import { ThemeProvider } from "./context/theme";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
+
+function App() {
+  const [themeMode, setThemeMode] = useState('light');
+  
+  const lightTheme = () =>{
+    setThemeMode('light')
+  }
+  const darkTheme = () =>{
+    setThemeMode('dark')
+  }
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+    
+  }, [themeMode])
+  
+
+  return (
+    <ThemeProvider value={{lightTheme, darkTheme, themeMode}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn/>
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
