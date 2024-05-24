66da87cb881a9e4ab7f52a5be85179bfca8ac1bc
vishal_rajput <rajputanavishal7@gmail.com>
2024-05-24 16:02:16 +0530

    started a megaProject

---
+++ 12blogapp/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  console.log(import.meta.env.VITE_APPWRITE_PROJECT_ID);
+
+  return (
+    <>
+    <h1>Hello Chai Army</h1>
+    </>
+  )
+}
+
+export default App
