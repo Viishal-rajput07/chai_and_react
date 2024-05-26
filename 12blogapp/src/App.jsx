d54d3c413e189d7432059e463fde08a1a4d11a3d
vishal_rajput <rajputanavishal7@gmail.com>
2024-05-26 12:30:55 +0530

    added redux-toolkit and tailwind

--- 12blogapp/src/App.jsx
+++ 12blogapp/src/App.jsx
@@ -1,14 +1,41 @@
-
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  console.log(import.meta.env.VITE_APPWRITE_PROJECT_ID);
+    const[loading, setLoading] = useState(true)
 
+    const dispatch = useDispatch()
+    useEffect(()=>{
+      authService.getCurrentUser()
+        .then((userData)=>{
+          if(userData){
+            dispatch(login({userData}))
+          }
+          else{
+            dispatch(logout())
+          }
+        })
+
+        .finally(()=> setLoading(false))
+    }, [])
   return (
-    <>
-    <h1>Hello Chai Army</h1>
-    </>
+    !loading ? (
+      <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        TODO:  <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+    )
+    : null
   )
 }
 
