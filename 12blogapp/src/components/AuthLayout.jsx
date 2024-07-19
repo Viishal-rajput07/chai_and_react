b47a9ad00cd9a1c62a4fe33dbd6b68f6472ae160
vishal_rajput <rajputanavishal7@gmail.com>
2024-07-19 15:27:30 +0530

    pages added

---
+++ 12blogapp/src/components/AuthLayout.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+export default function Protected({ children, authentication = true }) {
+  const [loader, setLoader] = useState(true);
+  const navigate = useNavigate();
+  const authStatus = useSelector((state) => state.auth.status);
+
+  useEffect(() => {
+    //TODO: make it more easy to understand
+
+    // if (authStatus ===true){
+    //     navigate("/")
+    // } else if (authStatus === false) {
+    //     navigate("/login")
+    // }
+
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+    setLoader(false);
+  }, [authStatus, navigate, authentication]);
+  return loader ? <h1>loading... </h1> : <>{children}</>;
+}
--- 12blogapp/src/components/index.js
+++ 12blogapp/src/components/index.js
@@ -1,7 +1,18 @@
 import Header from "./Header/Header";
-import Footer from "./Footer.jsx/Footer";
+import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Login from "./Login";
+import Signup from "./Signup";
+import Protected from "./AuthLayout";
+import Button from "./Button";
+import Input from './Input'
+import PostCard from "./PostCard";
+import Select from "./Select";
+import PostForm from "./post-form/PostForm";
+ 
 
-export {Header, Footer, Logo, LogoutBtn, Container}
+
+export {Header, Footer, Logo, LogoutBtn, Container, RTE, Login, Signup, Protected, Button, Select, Input, PostCard, PostForm}
